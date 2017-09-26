9b7eecbbae792df9f123cef325aa253028fce78e
Henrik <henrikbossart@me.com>
2017-09-26 10:18:09 +0200

    created a very simple navbar. mostly for experimenting with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './scripts/app';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import NavComponent from './scripts/navbar'
 import registerServiceWorker from './scripts/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavComponent />, document.querySelector('navbar'));
 registerServiceWorker();
