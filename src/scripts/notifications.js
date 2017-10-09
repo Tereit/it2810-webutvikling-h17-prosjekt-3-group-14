333487370cf43e6df5987c4b814f79c7d4ec8019
henrikbossart <henrikbossart@me.com>
2017-10-09 15:21:46 +0200

    working on notification system

--- src/scripts/notifications.js
+++ src/scripts/notifications.js
@@ -4,9 +4,16 @@ import NotificationSystem from 'react-notification-system';
 class Notifications extends Component {
   constructor(props) {
     super(props);
-    this.state = {recieveMessage: true, date: new Date()};
+    this.state = {
+      recieveMessage: true,
+      date: new Date(),
+      automaticNotifications: [],
+      title: '',
+      message: '',
+      notificationTime: null
+    };
+    // Notification list to display on screen
     this.notifications = null;
-    this.automaticNotifications = {};
 
     //Bindings
     this.handleChange = this.handleChange.bind(this);
@@ -16,112 +23,176 @@ class Notifications extends Component {
     this.addNotification = this.addNotification.bind(this);
     this.tick = this.tick.bind(this);
     this.automaticNotification = this.automaticNotification.bind(this);
+    this.createNotification = this.createNotification.bind(this);
+    this.buildNotification = this.buildNotification.bind(this);
+    this.saveNotifications = this.saveNotifications.bind(this);
+    this.getNotifications = this.getNotifications.bind(this);
+    this.parseDate = this.parseDate.bind(this);
+    this.logState = this.logState.bind(this);
 
   }
 
-  componentDidMount(){
-    this.getLocalState();
+  componentDidMount() {
+    this.getLocalState('recieveMessage');
     this.notifications = this.refs.notifications;
-    this.timerID = setInterval(
-      () => this.tick(),
-       1000
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
-  componentWillUnmount(){
+  componentWillMount() {
+    let notifications = this.getNotifications();
+    this.setState({automaticNotification: notifications});
+
+  }
+
+  componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
-  addNotification(){
-    console.log('i am called');
+  logState() {
+    console.log('state: ', this.state.automaticNotifications);
+  }
+
+  addNotification() {
     if (this.state.recieveMessage) {
-      //event.preventDefault();
-      this.notifications.addNotification({
-        title: 'Notification',
-        message: 'Test',
-        level: 'info',
-        position: 'br',
-        children: (
+      this.notifications.addNotification({title: 'Notification', message: 'Test', level: 'info', position: 'tr', children: (
           <div>
             <h2>Test</h2>
           </div>
-        )
-      });
+        )});
     }
   }
 
-  automaticNotification(){
-    const testDate = new Date(2017, 4, 9, 11, 46, 15);
-    if (this.state.date.toLocaleTimeString() ===
-        testDate.toLocaleTimeString()) {
-          console.log('time is equal');
-          this.addNotification();
+  automaticNotification() {
+    if (this.state.recieveMessage) {
+      for (var i = 0; i < this.state.automaticNotifications.length; i++) {
+        if (this.checkTime(this.state.automaticNotifications[i])) {
+          this.notifications.addNotification(this.state.automaticNotifications[i]);
+          this.state.automaticNotifications.splice([i], 1);
+          console.log('Removed notification ', [i]);
+        }
+      }
     }
   }
 
-  tick(){
+  createNotification(title, message, time) {
+    const notification = {
+      title: title,
+      message: message,
+      level: 'info',
+      position: 'br',
+      time: time
+    }
     this.setState({
-      date: new Date()
+      automaticNotifications: [
+        ...this.state.automaticNotifications,
+        notification
+      ]
     });
+    console.log('Notification created!', notification);
+    this.saveNotifications();
+  }
+
+  saveNotifications() {
+    localStorage.setItem('notifications', JSON.stringify(this.state.automaticNotifications));
+    console.log('Notifications saved!');
+  }
+
+  getNotifications() {
+    let notifications = [];
+    let inputString = JSON.parse(localStorage.getItem('notifications'));
+    for (var i = 0; i < inputString.length; i++) {
+      let time = this.parseDate(inputString[i].time.split('T'));
+      let notification = {
+        title: inputString[i].title,
+        message: inputString[i].message,
+        level: inputString[i].level,
+        position: inputString[i].position,
+        time: time
+      }
+      notifications.push(notification);
+    }
+
+    console.log('notifications: ', notifications);
+    console.log('Notifications loaded!');
+
+    return notifications;
+  }
+
+  parseDate(dateinfo) {
+    dateinfo = dateinfo[0].replace('"', '');
+    dateinfo = dateinfo.split('-');
+    return new Date(dateinfo[0], dateinfo[1] - 1, parseInt(dateinfo[2]));
+  }
+
+  tick() {
+    // Makes sure that the document has an updated date
+    this.setState({date: new Date()});
     this.automaticNotification();
   }
 
-  getLocalState(){
-    const localstate = localStorage.getItem('recieveMessage');
+  getLocalState(key) {
+    // Checks the browsers HTML localstorage for a state of recieveMessage.
+    const localstate = localStorage.getItem(key);
     if (localstate === 'true') {
-      this.setState({
-        recieveMessage: true
-      });
+      this.setState({recieveMessage: true});
     } else if (localstate === 'false') {
-      this.setState({
-        recieveMessage: false
-      });
+      this.setState({recieveMessage: false});
+    }
   }
-}
-
-  setLocalState(state){
-    localStorage.setItem('recieveMessage', state);
+  setLocalState(key, state) {
+    // Sets the HTML localStorage state of recieveMessage to the correct state
+    localStorage.setItem(key, state);
   }
 
-  checkTime(notificationTime){
+  checkTime(notification) {
     //Requires a date object
-    var notifDate = notificationTime.getDate();
-    var notifMin = notificationTime.getMinutes();
-    var notifHours = notificationTime.getHours();
+    let notifDate = notification.time.getDate();
+    let notifMin = notification.time.getMinutes();
+    let notifHours = notification.time.getHours();
 
-    if (notifDate === this.state.date.getDate() &&
-        notifMin === this.state.date.getMinutes() &&
-        notifHours === this.state.date.getHours()) {
+    if (notifDate === this.state.date.getDate() && notifMin === this.state.date.getMinutes() && notifHours === this.state.date.getHours()) {
       return true;
     } else {
       return false;
     }
   }
 
-  handleChange(event){
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-    this.setLocalState(value);
+  buildNotification() {
+    this.createNotification(this.state.title, this.state.message, new Date(this.state.notificationTime));
   }
 
-  render(){
-    return(
+  handleChange(event) {
+    // Changes the state of react component, and in the localstorage
+    const target = event.target;
+    const value = target.type === 'checkbox'
+      ? target.checked
+      : target.value;
+    const name = target.name;
+
+    this.setState({[name]: value});
+    this.setLocalState('recieveMessage', value);
+  }
+
+  render() {
+    return (
       <div>
         <h1>TIME: {this.state.date.toLocaleTimeString()}</h1>
-        <br />
-        <input
-        type="checkbox"
-        name="recieveMessage"
-        checked={this.state.recieveMessage}
-        onChange={this.handleChange} />
-        Recieve Messages?<br />
+        <br/>
+        <input type="checkbox" name="recieveMessage" checked={this.state.recieveMessage} onChange={this.handleChange}/>
+        Recieve Messages?<br/>
         <button onClick={this.addNotification}>test</button>
-        <NotificationSystem ref="notifications" />
+        <NotificationSystem ref="notifications"/>
+        <br/>
+        <input type="text" name="title" placeholder="title" onChange={this.handleChange}/>
+        <br/>
+        <br/>
+        <input type="text" name="message" placeholder="message" onChange={this.handleChange}/>
+        <br/>
+        <br/>
+        <input type="datetime" name="notificationTime" onChange={this.handleChange}/>
+        <br/>
+        <button onClick={this.buildNotification}>Add Notification</button>
+        {this.logState()}
       </div>
     );
   }
