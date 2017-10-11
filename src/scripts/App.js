d92fc6d66569beeddb99dc8d057cff353266ca1d
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-10-11 15:30:59 +0200

    Created a homepage with links to other components updated navbar to link to appropriate components (missing todo)

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -6,10 +6,81 @@ import Notes from './Notes';
 import Calendar from './Calendar';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            displayCalendar: false,
+            displayNotes: false,
+            displayTodo: false,
+            displayHome: true
+        };
+        this.display = this.display.bind(this);
+        this.setDisplay = this.setDisplay.bind(this);
+        this.displayCalendar = this.displayCalendar.bind(this);
+        this.displayNotes = this.displayNotes.bind(this);
+        this.displayTodo = this.displayTodo.bind(this);
+    }
+
+    setDisplay(display) {
+        this.setState({
+            displayCalendar: display.displayCalendar,
+            displayNotes: display.displayNotes,
+            displayTodo: display.displayTodo,
+            displayHome: display.displayHome
+        })
+    }
+
+    displayCalendar() {
+        this.setState({
+            displayCalendar: true,
+            displayNotes: false,
+            displayTodo: false,
+            displayHome: false
+        })
+    }
+
+    displayNotes() {
+        this.setState({
+            displayCalendar: false,
+            displayNotes: true,
+            displayTodo: false,
+            displayHome: false
+        })
+    }
+
+    displayTodo() {
+        this.setState({
+            displayCalendar: false,
+            displayNotes: false,
+            displayTodo: true,
+            displayHome: false
+        })
+    }
+
+    display() {
+        if(this.state.displayHome) {
+            return (
+                <div className="homeMenu">
+                    <div className="menu-icon"><i className="fa fa-calendar fa-5x" onClick={this.displayCalendar} /></div>
+                    <div className="menu-icon"><i className="fa fa-sticky-note fa-5x" onClick={this.displayNotes} /></div>
+                    <div className="menu-icon"><i className="fa fa-list-ul fa-5x" onClick={this.displayTodo} /></div>
+                </div>)
+        } else if(this.state.displayCalendar) {
+            return <Calendar/>
+        } else if(this.state.displayNotes) {
+            return <Notes/>
+        } else if(this.state.displayTodo) {
+            return <div><p>Todo list will be here when done</p></div>
+        } else {
+            return <div><p>Couldn't load requested page, something went wrong!</p></div>
+        }
+    }
+
   render() {
     return (
       <div className="App">
-
+          <NavComponent setDisplay={this.setDisplay}/>
+          {this.display()}
       </div>
     );
   }
