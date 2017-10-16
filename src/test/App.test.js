961d9179d7cc8c95fa6e9c624e021f9f14c7dfd6
miichan <mii-ng3@hotmail.com>
2017-10-16 18:45:09 +0200

    We tried to test. Issues occured. DO NOT MERGE into master or any other branches :)

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 import App from '../scripts/app';
 import { expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import renderer from 'react-test-renderer';
+//
+global.jestExpect = global.expect;
+global.expect = chai.expect;
 
+Enzyme.configure({adapter: new Adapter() });
 
 it("1+1 = 2?", () => {
   expect(1+1).to.equal(2)
 });
+
+it('renders without crashing', () =>{
+  const div = document.createElement('div');
+  let testApp = new App();
+  testApp.state ={
+    displayCalendar: false,
+    displayNotes: false,
+    displayTodo: true,
+    displayHome: true
+  }
+  ReactDOM.render(<testApp/>, div);
+})
+
+it('shallow render', () =>{
+  const testapp = shallow(<App/>);
+})
+
+it('snapshot testing', ()=>{
+  const rendered = renderer.create(<App/>);
+  jestExpect(rendered.toJSON()).toMatchSnapshot();
+
+})
